a78b4e548c5f93afcb052bb68dad0a5c137cb166
Adriana-Drika <nmdrika@gmail.com>
2022-11-04 07:39:39 -0300

    Versão final da Aula 1 React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
 import React from "react";
 
-import Primeiro from './components/Primeiro';
-import ComParametro from './components/ComParametro';
-import ComFilhos from './components/ComFilhos';
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+import ComFilhos from './components/basicos/ComFilhos';
 import Card from './components/layout/Card';
-import Repeticao from './components/Repeticao';
-import Condicional from './components/Condicional';
+import Repeticao from './components/basicos/Repeticao';
+import Condicional from './components/basicos/Condicional';
+import CondicionalComIf from './components/basicos/CondicionalComIf';
 
 
 export default(props)=> (
    <div className="App">
-      <Card titulo="#05 Condicional">
-        <Condicional numero={10}></Condicional>
+      <Card titulo="#06 Condicional V2">
+        <CondicionalComIf numero={10}></CondicionalComIf>
+      </Card>
+      <Card titulo="#05 Condicional V1">
+        <Condicional numero={11}></Condicional>
       </Card>
       <Card titulo="#04 Repetição">
         <Repeticao></Repeticao> 
